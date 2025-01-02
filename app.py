522ac7cb899e54b319212ade2929befebf97ffe1
Darsh Milind Chavre <67714016+dchavre@users.noreply.github.com>
2025-01-02 16:33:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -423,4 +423,4 @@ def update():
     return jsonify({"message": "Table updated successfully!"})
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
